96e852cc875cb3ef847ae3bad16afd9bf673a410
Serhii Hordiienko <phrippy2@gmail.com>
2022-08-22 12:22:14 +0300

    upd

---
+++ lesson13/make.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd `dirname $0`
+(cat head.md ; echo ; echo '# Скрипт створення користувача' ; echo '```bash' ; cat user.sh; echo '```' ; echo ; echo '# Скрипт створення каталогу task' ; echo ; echo '```bash' ; cat task.sh ; echo '```' ) > lesson13.md
