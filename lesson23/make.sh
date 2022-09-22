c0615a8bf81986a6666c5fdf7200c2904ede9985
Сергій Гордієнко <phrippy2@gmail.com>
2022-09-22 18:06:19 +0300

    Develop (#30)
    Merge from develop

---
+++ lesson23/make.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+(
+cat head.md
+echo
+echo '```bash'
+cat keygen.sh
+echo '```'
+echo
+cat keygen.md
+echo
+echo '```bash'
+cat server.conf
+echo '```'
+echo
+cat client.md
+echo
+echo '```bash'
+cat client.conf
+echo '```'
+echo
+cat ovpn.md
+echo
+echo '```bash'
+cat ovpnmake.sh
+echo '```'
+echo
+cat ovpn-ready.md
+echo
+echo '```bash'
+cat firewall.sh
+echo '```'
+echo
+cat firewall.md
+) > lesson23.md
