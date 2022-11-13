6425ff087aebcf802728181078d8dc0ad150d716
Serhii Hordiienko <phrippy2@gmail.com>
2022-11-14 01:54:15 +0200

    change availability_zone to auto mode

--- lesson33/phonebook-terraform/phonebook.tf
+++ lesson33/phonebook-terraform/phonebook.tf
@@ -19,7 +19,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "192.168.8.0/24"
-  availability_zone = "us-west-2a"
+  # availability_zone = "eu-central-1a"
 
   tags = {
     Name = "lesson33"
