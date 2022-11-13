590b489207f7c03f1801bc5c79f5af655f297cbe
Serhii Hordiienko <phrippy2@gmail.com>
2022-11-14 01:41:28 +0200

    add funny info

--- lesson33/phonebook-terraform/phonebook.tf
+++ lesson33/phonebook-terraform/phonebook.tf
@@ -1,4 +1,5 @@
 provider "aws" {
+  # Obviously, this keys are invalid 🤓
   # export AWS_ACCESS_KEY_ID = "AKIAXBMESHIKJH2WHGMC"
   # export AWS_SECRET_ACCESS_KEY = "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"
   # export AWS_DEFAULT_REGION = "eu-central-1"
