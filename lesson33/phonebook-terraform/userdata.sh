d25976c5bae3792970d5c11c803657e52b40ba08
Serhii Hordiienko <phrippy2@gmail.com>
2022-11-14 07:00:57 +0200

    update userdata from https://raw.githubusercontent.com/aws-samples/simple-phonebook-web-application/master/CloudFormation.yaml

--- lesson33/phonebook-terraform/userdata.sh
+++ lesson33/phonebook-terraform/userdata.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 sudo su
-yum update -y
-yum install -y httpd
-systemctl start httpd.service
-systemctl enable httpd.service
-echo -n "xxx" > /var/www/html/index.html
+# yum update -y
+# yum install -y httpd
+# systemctl start httpd.service
+# systemctl enable httpd.service
+# echo -n "xxx" > /var/www/html/index.html
+
+yum -y install httpd php mysql php-mysql
+
+case $(ps -p 1 -o comm | tail -1) in
+systemd) systemctl enable --now httpd ;;
+init)
+    chkconfig httpd on
+    service httpd start
+    ;;
+*) echo "Error starting httpd (OS not using init or systemd)." 2>&1 ;;
+esac
+
+if [ ! -f /var/www/html/bootcamp-app.tar.gz ]; then
+    cd /var/www/html
+    wget https://s3.amazonaws.com/immersionday-labs/bootcamp-app.tar
+    tar xvf bootcamp-app.tar
+    chown apache:root /var/www/html/rds.conf.php
+fi
+yum -y update
