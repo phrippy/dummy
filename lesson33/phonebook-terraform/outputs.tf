5302fb40259732d6d53ce0357611cb32f8413821
Serhii Hordiienko <phrippy2@gmail.com>
2022-11-14 03:55:42 +0200

    update outputs.tf

--- lesson33/phonebook-terraform/outputs.tf
+++ lesson33/phonebook-terraform/outputs.tf
@@ -5,3 +5,11 @@ output "instance_ip_addr" {
 output "instance_dns" {
   value = aws_instance.phonebook_instance.public_dns
 }
+
+output "eip_instance_ip_addr" {
+  value = aws_eip.my_eip.public_ip
+}
+
+output "eip_instance_dns" {
+  value = aws_eip.my_eip.public_dns
+}
