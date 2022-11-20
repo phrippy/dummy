8c021557a357e886c8fd8197ca1d2fa6eca0bec4
Serhii Hordiienko <phrippy2@gmail.com>
2022-11-20 07:47:07 +0200

    delete ip and dns from outputs, rest only elastic

--- lesson33/phonebook-terraform/outputs.tf
+++ lesson33/phonebook-terraform/outputs.tf
@@ -1,10 +1,10 @@
-output "instance_ip_addr" {
-  value = aws_instance.phonebook_instance.public_ip
-}
+# output "instance_ip_addr" {
+#   value = aws_instance.phonebook_instance.public_ip
+# }
 
-output "instance_dns" {
-  value = aws_instance.phonebook_instance.public_dns
-}
+# output "instance_dns" {
+#   value = aws_instance.phonebook_instance.public_dns
+# }
 
 output "eip_instance_ip_addr" {
   value = aws_eip.my_eip.public_ip
