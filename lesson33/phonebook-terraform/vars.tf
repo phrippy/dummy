ebd6791f48af6558d2fb862f19d317e2ab631262
Serhii Hordiienko <phrippy2@gmail.com>
2022-11-20 07:41:49 +0200

    add var.tf

---
+++ lesson33/phonebook-terraform/vars.tf
@@ -0,0 +1,14 @@
+variable "db_user" {
+  type    = string
+  default = "foo"
+}
+
+variable "db_pass" {
+  type    = string
+  default = "foobarbaz"
+}
+
+variable "db_name" {
+  type    = string
+  default = "mydb"
+}
