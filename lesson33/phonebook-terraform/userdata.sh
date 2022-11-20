885941a4a3e0d54beccc0cce773cda2c82cd4786
Serhii Hordiienko <phrippy2@gmail.com>
2022-11-20 11:57:28 +0200

    update userdata

--- lesson33/phonebook-terraform/userdata.sh
+++ lesson33/phonebook-terraform/userdata.sh
@@ -11,22 +11,23 @@ yum -y install httpd php mysql php-mysql
 
 systemctl enable --now httpd
 
-echo until mysql --host ${host} --user ${user} --password ${pass} --port ${port} -e \'SELECT 1;\' >> /log.txt
-until mysql --host ${host} --user ${user} --password ${pass} --port ${port} -e \'SELECT 1;\'; do date >> /log.txt; done
-echo success >> /log.txt
+echo until mysql --host ${host} --user ${user} --password ${pass} --port ${port} -e "'SELECT 1;'" >>/log.txt
+until
+    mysql --host ${host} --user ${user} --password ${pass} --port ${port} -e "'SELECT 1;'" ; do date >>/log.txt; done
+echo success >>/log.txt
 echo mysql --host ${host} \
     --user=${user} \
     --password=${pass} \
     --port=${port} \
     ${name} \
-    -e \'status\' >> /log.txt
+    -e \'status\' >>/log.txt
 
 mysql --host ${host} \
     --user=${user} \
     --password=${pass} \
     --port=${port} \
     ${name} \
-    -e \'status\' > /status.txt
+    -e \'status\' >/status.txt
 
 if [ ! -f /var/www/html/bootcamp-app.tar.gz ]; then
     cd /var/www/html
@@ -36,8 +37,8 @@ if [ ! -f /var/www/html/bootcamp-app.tar.gz ]; then
 <?php \$RDS_URL='${host}'; \$RDS_DB='${name}'; \$RDS_user='${user}'; \$RDS_pwd='${pass}'; ?>
 EOF
     chown apache:root /var/www/html/rds.conf.php
-    mysql --host ${host} --user ${user} --password ${pass} --port ${port} < /var/www/html/sql/addressbook.sql
-    echo $? > /result.txt
+    mysql --host ${host} --user ${user} --password ${pass} --port ${port} </var/www/html/sql/addressbook.sql
+    echo $? >/result.txt
 fi
 # yum -y update
 # EOF
