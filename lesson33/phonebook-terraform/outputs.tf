cfd5137113d391b6835efe30fceda1b5d759e040
Serhii Hordiienko <phrippy2@gmail.com>
2022-11-20 06:39:46 +0200

    update outputs.tf

--- lesson33/phonebook-terraform/outputs.tf
+++ lesson33/phonebook-terraform/outputs.tf
@@ -13,3 +13,23 @@ output "eip_instance_ip_addr" {
 output "eip_instance_dns" {
   value = aws_eip.my_eip.public_dns
 }
+
+output "db_address" {
+  value = aws_db_instance.database.address
+}
+
+output "db_port" {
+  value = aws_db_instance.database.port
+}
+
+output "db_user" {
+  value = aws_db_instance.database.username
+}
+
+output "db_pass" {
+  value = aws_db_instance.database.pass
+}
+
+output "db_name" {
+  value = aws_db_instance.database.name
+}
